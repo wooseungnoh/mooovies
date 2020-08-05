e9f0b5aec52c23aa0b40776c72c0a65993ee9019
wooseung <shdntmd44@naver.com>
2020-08-05 19:54:37 +0900

    create Tab navigation

--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Detail from "../screens/Detail";
-import Home from "../screens/Home";
+import Home from "../screens/Movies";
+import Tabs from "./Tabs";
 
 const Stack = createStackNavigator();
 
 export default () => (<Stack.Navigator>
-    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="Tabs" component={Tabs} />
     <Stack.Screen name="Detail" component={Detail} />
   </Stack.Navigator>)
